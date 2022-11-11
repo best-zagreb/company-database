72cd0889f9b6d7d2319c55978255a95bf3544543
CrazyFreak <jakovbla4@gmail.com>
2022-11-11 14:00:34 +0100

    Added validation and improved UX in user form

--- Frontend/src/components/forms/UserForm.jsx
+++ Frontend/src/components/forms/UserForm.jsx
@@ -6,7 +6,6 @@ import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Fade from "@mui/material/Fade";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 
@@ -27,22 +26,154 @@ const authLevels = [
   },
 ];
 
+const user = {
+  name: "",
+  surname: "",
+  nickname: "",
+  loginEmail: "",
+  notificationEmail: "",
+  authLevel: "",
+  description: "",
+};
+
+function ValidateEmail(inputEmail) {
+  const mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputEmail.match(mailformat)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export default function UserForm() {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(true);
 
+  const [name, setName] = useState();
+  const [surname, setSurname] = useState();
+  const [nickname, setNickname] = useState();
+  const [loginEmail, setLoginEmail] = useState();
+  const [notificationEmail, setNotificationEmail] = useState();
   const [authLevel, setAuthLevel] = useState("Observer");
-  const handleChange = (event) => {
-    setAuthLevel(event.target.value);
+  const [description, setDescription] = useState();
+
+  const [nameIsValid, setNameIsValid] = useState(false);
+  const [nameDirty, setNameDirty] = useState(false);
+  const handleNameChange = (e) => {
+    const input = e.target.value;
+    if (input.length >= 2 && input.length <= 35) {
+      setNameIsValid(true);
+    } else {
+      setNameIsValid(false);
+    }
+
+    setName(input);
+  };
+  const [surnameIsValid, setSurnameIsValid] = useState(false);
+  const [surnameDirty, setSurnameDirty] = useState(false);
+  const handleSurnameChange = (e) => {
+    const input = e.target.value;
+    if (input.length >= 2 && input.length <= 35) {
+      setSurnameIsValid(true);
+    } else {
+      setSurnameIsValid(false);
+    }
+
+    setSurname(input);
+  };
+  const [nicknameIsValid, setNicknameIsValid] = useState(true);
+  const handleNicknameChange = (e) => {
+    const input = e.target.value;
+    if (input.length <= 35) {
+      setNicknameIsValid(true);
+    } else {
+      setNicknameIsValid(false);
+    }
+
+    setNickname(input);
+  };
+  const [loginEmailIsValid, setLoginEmailIsValid] = useState(false);
+  const [loginEmailDirty, setLoginEmailDirty] = useState(false);
+  const handleLoginEmailChange = (e) => {
+    const input = e.target.value;
+    if (ValidateEmail(input)) {
+      setLoginEmailIsValid(true);
+    } else {
+      setLoginEmailIsValid(false);
+    }
+
+    setLoginEmail(input);
+  };
+  const [notificationEmailIsValid, setNotificationEmailIsValid] =
+    useState(false);
+  const [notificationEmailDirty, setNotificationEmailDirty] = useState(false);
+  const handleNotificationEmailChange = (e) => {
+    const input = e.target.value;
+    if (ValidateEmail(input)) {
+      setNotificationEmailIsValid(true);
+    } else {
+      setNotificationEmailIsValid(false);
+    }
+
+    setNotificationEmail(input);
+  };
+  const [authLevelIsValid, setAuthLevelIsValid] = useState(false);
+  const [authLevelDirty, setAuthLevelDirty] = useState(false);
+  const handleAuthLevelChange = (e) => {
+    const input = e.target.value;
+    if (
+      input === "Observer" ||
+      input === "Moderator" ||
+      input === "Administrator"
+    ) {
+      setAuthLevelIsValid(true);
+    } else {
+      setAuthLevelIsValid(false);
+    }
+
+    setAuthLevel(input);
+  };
+  const [descriptionIsValid, setDescriptionIsValid] = useState(true);
+  const handleDescriptionChange = (e) => {
+    const input = e.target.value;
+    if (input.length <= 475) {
+      setDescriptionIsValid(true);
+    } else {
+      setDescriptionIsValid(false);
+    }
+
+    setDescription(input);
   };
 
-  const [open, setOpen] = useState(true);
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      nameIsValid &&
+      surnameIsValid &&
+      nicknameIsValid &&
+      loginEmailIsValid &&
+      notificationEmailIsValid &&
+      authLevelIsValid &&
+      descriptionIsValid
+    ) {
+      user.name = name;
+      user.surname = surname;
+      user.nickname = nickname;
+      user.loginEmail = loginEmail;
+      user.notificationEmail = notificationEmail;
+      user.authLevel = authLevel;
+      user.description = description;
+      console.log(user);
+      // post user
+
+      handleClose();
+    }
+  };
+
   const handleClose = () => {
     setOpen(false);
     navigate("/users");
-  };
-  const handlePost = () => {
-    // post form
-    handleClose();
   };
 
   return (
@@ -61,11 +192,9 @@ export default function UserForm() {
       >
         <Fade in={open}>
           <Box className="Box">
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Add new user
-            </Typography>
+            <h2>Add new user</h2>
 
-            <form>
+            <form onSubmit={onSubmit}>
               <TextField
                 id="outlined"
                 label="Name"
@@ -74,7 +203,17 @@ export default function UserForm() {
                 fullWidth
                 margin="dense"
                 placeholder="Jane"
-                inputProps={{ minlength: 2, maxlength: 35 }}
+                inputProps={{ minLength: 2, maxLength: 35 }}
+                onBlur={() => {
+                  setNameDirty(true);
+                }}
+                error={nameDirty && !nameIsValid}
+                helperText={
+                  nameDirty &&
+                  !nameIsValid &&
+                  "Name must be between 2 and 35 characters"
+                }
+                onChange={handleNameChange}
               />
               <TextField
                 id="outlined"
@@ -84,7 +223,17 @@ export default function UserForm() {
                 fullWidth
                 margin="dense"
                 placeholder="Doe"
-                inputProps={{ minlength: 2, maxlength: 35 }}
+                inputProps={{ minLength: 2, maxLength: 35 }}
+                onBlur={() => {
+                  setSurnameDirty(true);
+                }}
+                error={surnameDirty && !surnameIsValid}
+                helperText={
+                  surnameDirty &&
+                  !surnameIsValid &&
+                  "Surname must be between 2 and 35 characters"
+                }
+                onChange={handleSurnameChange}
               />
               <TextField
                 id="outlined"
@@ -92,7 +241,12 @@ export default function UserForm() {
                 type="text"
                 fullWidth
                 margin="dense"
-                inputProps={{ minlength: 2, maxlength: 35 }}
+                inputProps={{ maxLength: 35 }}
+                error={!nicknameIsValid}
+                helperText={
+                  !nicknameIsValid && "Nickname must be under 35 characters"
+                }
+                onChange={handleNicknameChange}
               />
 
               <TextField
@@ -102,9 +256,18 @@ export default function UserForm() {
                 required
                 fullWidth
                 margin="dense"
-                helperText="User will login to CDB with this email"
                 placeholder="jane.doe@best.hr"
-                inputProps={{ minlength: 3, maxlength: 55 }}
+                inputProps={{ minLength: 6, maxLength: 55 }}
+                onBlur={() => {
+                  setLoginEmailDirty(true);
+                }}
+                error={loginEmailDirty && !loginEmailIsValid}
+                helperText={
+                  loginEmailDirty && !loginEmailIsValid
+                    ? "Invalid email or email length"
+                    : "User will login to CDB with this email"
+                }
+                onChange={handleLoginEmailChange}
               />
               <TextField
                 id="outlined"
@@ -113,9 +276,18 @@ export default function UserForm() {
                 required
                 fullWidth
                 margin="dense"
-                helperText="Notifications will be sent to this email"
                 placeholder="jane.doe@gmail.com"
-                inputProps={{ minlength: 3, maxlength: 55 }}
+                inputProps={{ minLength: 6, maxLength: 55 }}
+                onBlur={() => {
+                  setNotificationEmailDirty(true);
+                }}
+                error={notificationEmailDirty && !notificationEmailIsValid}
+                helperText={
+                  notificationEmailDirty && !notificationEmailIsValid
+                    ? "Invalid email or email length"
+                    : "App notifications will be sent to this email"
+                }
+                onChange={handleNotificationEmailChange}
               />
 
               <TextField
@@ -126,7 +298,16 @@ export default function UserForm() {
                 fullWidth
                 margin="dense"
                 value={authLevel}
-                onChange={handleChange}
+                onBlur={() => {
+                  setAuthLevelDirty(true);
+                }}
+                error={authLevelDirty && !authLevelIsValid}
+                helperText={
+                  authLevelDirty &&
+                  !authLevelIsValid &&
+                  "Invalid authorization level"
+                }
+                onChange={handleAuthLevelChange}
               >
                 {authLevels.map((option) => (
                   <MenuItem key={option.value} value={option.value}>
@@ -143,7 +324,12 @@ export default function UserForm() {
                 minRows={2}
                 maxRows={4}
                 margin="dense"
-                inputProps={{ maxlength: 475 }}
+                inputProps={{ maxLength: 475 }}
+                error={!descriptionIsValid}
+                helperText={
+                  !descriptionIsValid && "Nickname must be under 475 characters"
+                }
+                onChange={handleDescriptionChange}
               />
 
               <div className="action-btns">
@@ -151,7 +337,7 @@ export default function UserForm() {
                   Cancel
                 </Button>
 
-                <Button variant="contained" onClick={handlePost}>
+                <Button variant="contained" type="submit">
                   Add user
                 </Button>
               </div>
@@ -164,9 +350,6 @@ export default function UserForm() {
 }
 
 // export default function NewUser() {
-//   const [name, setName] = useState();
-//   const navigate = useNavigate();
-
 //   // TODO: post to correct URL
 //   const namePost = async () => {
 //     await fetch("http://localhost:8080/users/add-user", {
@@ -180,24 +363,11 @@ export default function UserForm() {
 //   };
 
 //   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     console.log("form submited " + name);
 //     namePost().then((res) => {
 //       // display success or error msg
 //       navigate("/users");
 //     });
 //   };
 
-//   return (
-//     <>
-//       <h1>New user form</h1>
-
-//       {/* TODO: add form with inputs for all data */}
-//       <form onSubmit={handleSubmit}>
-//         <input onChange={(e) => setName(e.target.value)} value={name}></input>
-//         <button type="submit">Click to submit</button>
-//       </form>
-//     </>
 //   );
 // }
