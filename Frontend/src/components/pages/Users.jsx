9f8cecf3c4f77ca13d09e7ed8bb9c8cafb002ec3
Ivor Isus <ivor.baricevic3gmail.com>
2022-11-11 13:07:30 +0100

    added user table and prepared functionalities for delete and edit button,added search interface with no functions

--- Frontend/src/components/pages/Users.jsx
+++ Frontend/src/components/pages/Users.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { usersData } from "../../data/users"; // temp data created with mockaroo
-
+import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
+import {Box, TextField,TableCell, TableHead,Paper,TableContainer,TableRow,TableBody,Table } from "@mui/material";
+
 
 export default function Users() {
   // TODO: add correct url to get data from
@@ -18,32 +20,99 @@ export default function Users() {
   //   fetchUsers();
   // }, []);
 
+
+  //zanemari endpoint ceka se zadnja verzija backenda
+  const deleteUser = async (userid) => {
+    await fetch("http://localhost:8080/users/delete-user/"+userid, {
+      method: "POST"
+     
+      
+    });
+  };
+
+  const handleDelete = (e,userid) => {
+    e.preventDefault();
+
+    console.log("user"+userid +" Deleted");
+    deleteUser(userid).then((res) => {
+      // display success or error msg
+      navigate("/users");
+    });
+  };
+  let navigate = useNavigate();
+
+  function editHandler(e,userid) {
+    e.preventDefault()
+    
+
+   
+        navigate(`edit/${userid}`);
+     
+  }
+
+  function search(e) {
+    e.preventDefault()
+    console.log(document.getElementById('search').value)
+    
+
+
+  }
+  
+
   return (
     <>
       <Link to="new">
-        <Button variant="contained" size="large">
+        <Button variant="contained" size="large"  >
           New user
         </Button>
       </Link>
-      {/* 
-      <Link to="edit/1">
-        <Button variant="outlined" size="large">
-          TEST Edit user
+     <Box m={1}>
+      <TextField id="search" label="Search" variant="filled" />
+      <Button variant="contained" size="small" onClick={(e)=>search(e)}  >
+          Search
         </Button>
-      </Link>
-
-      <Link to="delete/1">
-        <Button variant="outlined" size="large">
-          TEST Delete user
-        </Button>
-      </Link> */}
-
-      <ul>
-        {usersData.map((user) => (
-          // TODO: display in table and add in each row edit and delete button
-          <li key={user.id}>{user.nickname}</li>
-        ))}
-      </ul>
+        </Box>
+   
+    
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+            <TableCell>User Nickname</TableCell>
+            <TableCell>Edit User</TableCell>
+            <TableCell>Delete User</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {usersData.map((user) => (
+            <TableRow key={user.id} className={user.nickname}>
+              <TableCell>
+                {user.nickname}
+              </TableCell>
+              <TableCell>
+                <Button variant="outlined" size="large"
+                  onClick={(e) => editHandler(e,user.id)}>
+                  Edit user
+                </Button>
+              </TableCell>
+              <TableCell>
+                <Button variant="outlined" size="large"  
+                  onClick={(e) => handleDelete(e,user.id)}>
+                    
+                  Delete user
+                </Button>
+              </TableCell>
+            </TableRow>
+             ))} 
+          </TableBody>
+        </Table>
+      </TableContainer>
+  
+      
+        
+      
+      
+      
     </>
   );
 }
