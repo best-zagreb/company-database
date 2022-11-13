cfec89d85250e3fda1e56e1357f1319257c6bc8f
JJ <jakovbla4@gmail.com>
2022-11-13 12:23:27 +0100

    Removed search button which was causing the app crashing problem

--- Frontend/src/components/search_bar/SearchBar.js
+++ Frontend/src/components/search_bar/SearchBar.js
@@ -1,41 +1,36 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons"
+const SearchBar = ({ posts, setSearchResults }) => {
+  const handleSubmit = (e) => e.preventDefault();
 
-const SearchBar = ({posts, setSearchResults}) => {
-    const handleSubmit = (e) => e.preventDefault();
-
-    const handleSearchChange = (e) => {
-        if(!e.target.value) {
-            console.log("tu sam")
-            return setSearchResults(posts) //ako nema nista u search baru renderaj samo sve sto je i bilo prije
-            
-        }
-
-        const resultsArray = posts.filter(post => {
-           return  post.nickname.includes(e.target.value) || post.name.includes(e.target.value)
-                || post.surname.includes(e.target.value) || post.loginEmail.includes(e.target.value)
-        }
-        )
-        console.log(resultsArray)
-        setSearchResults(resultsArray)
+  const handleSearchChange = (e) => {
+    if (!e.target.value) {
+      console.log("tu sam");
+      return setSearchResults(posts); //ako nema nista u search baru renderaj samo sve sto je i bilo prije
     }
 
-    return(
-        <header>
-            <form className = "search" onSubmit = {handleSubmit}>
-                <input 
-                    className = "search__input"
-                    type = "text"
-                    id = "search__bar"
-                    onChange = {handleSearchChange}
-                />
+    const resultsArray = posts.filter((post) => {
+      return (
+        post.nickname.includes(e.target.value) ||
+        post.name.includes(e.target.value) ||
+        post.surname.includes(e.target.value) ||
+        post.loginEmail.includes(e.target.value)
+      );
+    });
+    console.log(resultsArray);
+    setSearchResults(resultsArray);
+  };
 
-                <button  id = "search__button">
-                    <FontAwesomeIcon icon = {faMagnifyingGlass} />
-                </button>
-            </form>
-        </header>
-    )
-}
+  return (
+    <header>
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          className="search__input"
+          type="text"
+          id="search__bar"
+          onChange={handleSearchChange}
+        />
+      </form>
+    </header>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
