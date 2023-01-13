0b98bc4c8eb20830dff74f3824e5056d7455a15c
CrazyFreak <jakovbla4@gmail.com>
2023-01-13 20:54:09 +0100

    Created contact form

--- Frontend/src/components/forms/ContactForm.jsx
+++ Frontend/src/components/forms/ContactForm.jsx
@@ -4,57 +4,90 @@ import { Backdrop, Box, Modal, Fade, Button, TextField } from "@mui/material";
 
 import "./Form.css";
 
-const contact = {
-  firstName: null,
-  lastName: null,
-  email: null,
-  tel: null,
-  position: null,
-  description: null,
-};
-
 export default function ContactForm({
   openModal,
   setOpenModal,
   fetchContacts,
+  company,
+  contact,
 }) {
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    contact.firstName = name;
-    contact.lastName = surname;
-    contact.email = nickname;
-    contact.tel = loginEmail;
-    contact.position = notificationEmail;
-    contact.description = description;
-    // console.log(user);
-
-    const JWToken = JSON.parse(localStorage.getItem("loginInfo")).JWT;
-    fetch("http://159.65.127.217:8080/companies/" + company.id + "/contacts", {
-      method: "POST",
-      headers: {
-        googleTokenEncoded: JWToken.credential,
-
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(contact),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        // if error display error toast
-        console.log(json);
-
-        // if success display success toast, close modal and update contacts list
-        setOpenModal(false);
-      });
-  };
-
   const [name, setName] = useState();
   const [surname, setSurname] = useState();
   const [email, setEmail] = useState();
   const [tel, setTel] = useState();
   const [position, setPosition] = useState();
   const [description, setDescription] = useState();
+
+  const contactTemplate = {
+    firstName: null,
+    lastName: null,
+    email: null,
+    tel: null,
+    position: null,
+    description: null,
+  };
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    contactTemplate.firstName = name;
+    contactTemplate.lastName = surname;
+    contactTemplate.email = email;
+    contactTemplate.tel = tel;
+    contactTemplate.position = position;
+    contactTemplate.description = description;
+    // console.log(contactTemplate);
+
+    const JWToken = JSON.parse(localStorage.getItem("loginInfo")).JWT;
+    // fetch("http://159.65.127.217:8080/companies/" + company.id + "/contacts", {
+    //   method: "POST",
+    //   headers: {
+    //     googleTokenEncoded: JWToken.credential,
+
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(contact),
+    // })
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     // if error display error toast
+    //     console.log(json);
+
+    //     // if success display success toast, close modal and update contacts list
+    //     setOpenModal(false);
+    //   });
+  }
+
+  const handleNameChange = (e) => {
+    const input = e.target.value;
+
+    setName(input);
+  };
+  const handleSurnameChange = (e) => {
+    const input = e.target.value;
+
+    setSurname(input);
+  };
+  const handleEmailChange = (e) => {
+    const input = e.target.value;
+
+    setEmail(input);
+  };
+  const handleTelChange = (e) => {
+    const input = e.target.value;
+
+    setTel(input);
+  };
+  const handlePositionChange = (e) => {
+    const input = e.target.value;
+
+    setPosition(input);
+  };
+  const handleDescriptionChange = (e) => {
+    const input = e.target.value;
+
+    setDescription(input);
+  };
 
   return (
     <div>
@@ -78,7 +111,6 @@ export default function ContactForm({
 
             <form onSubmit={onSubmit}>
               <TextField
-                id="outlined"
                 label="Name"
                 type="text"
                 required
@@ -86,10 +118,52 @@ export default function ContactForm({
                 margin="dense"
                 placeholder="Jane"
                 inputProps={{ minLength: 2, maxLength: 35 }}
+                onChange={handleNameChange}
               />
 
               <TextField
-                id="outlined-multiline-static"
+                label="Surname"
+                type="text"
+                required
+                fullWidth
+                margin="dense"
+                placeholder="Doe"
+                inputProps={{ minLength: 2, maxLength: 35 }}
+                onChange={handleSurnameChange}
+              />
+
+              <TextField
+                label="Email"
+                type="text"
+                required
+                fullWidth
+                margin="dense"
+                placeholder="jane.doe@best.hr"
+                inputProps={{ minLength: 2, maxLength: 35 }}
+                onChange={handleEmailChange}
+              />
+
+              <TextField
+                label="Tel"
+                type="text"
+                fullWidth
+                margin="dense"
+                placeholder="0987654321"
+                inputProps={{ minLength: 2, maxLength: 35 }}
+                onChange={handleTelChange}
+              />
+
+              <TextField
+                label="Position"
+                type="text"
+                fullWidth
+                margin="dense"
+                placeholder="PR"
+                inputProps={{ minLength: 2, maxLength: 35 }}
+                onChange={handlePositionChange}
+              />
+
+              <TextField
                 label="Description"
                 fullWidth
                 multiline
@@ -97,6 +171,7 @@ export default function ContactForm({
                 maxRows={4}
                 margin="dense"
                 inputProps={{ maxLength: 475 }}
+                onChange={handleDescriptionChange}
               />
 
               <div className="action-btns">
