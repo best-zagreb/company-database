1d4c1d12c8b361f703e92ff0e04e01c340307dfe
JJ <jakovbla4@gmail.com>
2023-08-23 07:59:07 +0200

    Typo in comment caused by my recent commit (domain to sector)

--- Frontend/src/components/forms/CompanyForm.jsx
+++ Frontend/src/components/forms/CompanyForm.jsx
@@ -521,7 +521,7 @@ export default function CompanyForm({
                 validationFunction={(input) => {
                   const urlPattern = new RegExp(
                     "^(https?:\\/\\/)?" + // validate protocol
-                      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate sector name
+                      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
                       "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
                       "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
                       "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
